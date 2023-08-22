b56832120f1846337ddc575bf90bb66b270d2ee8
Rinn <hidehastaken@gmail.com>
2023-08-22 15:21:39 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,86 @@
+import os, sys, time, random
+Y = '\033[93m' #Yellow
+P = '\033[95m' #Purple
+B = '\033[94m' #Dark blue
+R = '\033[91m' #Red
+G = '\033[92m' #Green
+time.sleep(2)
+#pertanyaan 1
+os.system('clear')
+print("Anime Terbaik \n*hasil ini menurut MyAnimeList")
+time.sleep('1')
+q1 = input(B+("\na.Chainsaw Man\nb.Black Clover\nc.One Piece\nJawab "))
+os.system('clear')
+print(R+("CALCULATING............."))
+time.sleep(3)
+os.system('clear')
+if q1 == "c":
+     print(G+("Kamu betul! Kamu akan dialihkan ke pertanyaan\nselanjutnya!"))
+else:
+     print(R+("Kamu Salah! Kamu akan dialihkan ke pertanyaan\nselanjutnya!"))
+
+time.sleep(3)
+#pertanyaan 2
+os.system('clear')
+print("Waifu Terbaik Dalam Anime \n*hasil ini menurut MyAnimeList")
+time.sleep('1')
+q2 = input(B+("\na.Tokisaki Kurumi\nb.Makima\nc.Rias Gremory\nJawab "))
+os.system('clear')
+print(R+("CALCULATING.........."))
+time.sleep(3)
+os.system('clear')
+if q2 == "b":
+     print(G+("Kamu Betul Sekali!  Kamu akan dialihkan ke pertanyaan\nselanjutnya!"))
+else:
+     print(R+("Salah! Kamu akan dialihkan ke pertanyaan\nselanjutnya!"))
+
+
+time.sleep(3)
+#pertanyaan 3
+os.system('clear')
+print("Femboy Terbaik Dalamm Anime \n*hasil ini menurut MyAnimeList")
+time.sleep('1')
+q3 = input(B+("\na.Rimuru\nb.Astolfo\nc.Felix\nJawab "))
+os.system('clear')
+print(R+("CALCULATING.........."))
+time.sleep(3)
+os.system('clear')
+if q3 == "a":
+     print(G+("Kamu Betul Sekali!  Kamu akan dialihkan ke pertanyaan\nselanjutnya!"))
+else:
+     print(R+("Salah! Kamu akan dialihkan ke pertanyaan\nselanjutnya!"))
+
+time.sleep(3)
+#pertanyaan 4
+os.system('clear')
+print("Author Anime Your Name")
+time.sleep('1')
+q4 = input(B+("\na.Hideo Kojima\nb.Makoto Shinkai\nc.Hajime Isayama\nJawab "))
+os.system('clear')
+print(R+("CALCULATING.........."))
+time.sleep(3)
+os.system('clear')
+if q4 == "c":
+     print(G+("Kamu Betul Sekali! Kamu akan dialihkan ke pertanyaan\nselanjutnya!"))
+else:
+     print(R+("Salah! Kamu akan dialihkan ke pertanyaan\nselanjutnya!"))
+
+
+
+time.sleep(3)
+#pertanyaan 5
+os.system('clear')
+print("Studio Yang Membuat Demon Slayer")
+time.sleep('1')
+q5 = input(B+("\na.Mappa\nb.Ufotable\nc.MadHouse\nJawab "))
+os.system('clear')
+print(R+("CALCULATING.........."))
+time.sleep(3)
+os.system('clear')
+if q5 == "b":
+     print(G+("Kamu Betul Sekali! Kamu akan dialihkan ke pertanyaan\nselanjutnya!"))
+else:
+     print(R+("Salah! Kamu akan dialihkan ke pertanyaan\nselanjutnya!"))
+
+time.sleep(4)
+x = input(R+("\n\nenter untuk keluar"))
